a8df3f06a5d42707921616f59f7103e7f19f0111
RAMJAN <niloyahmadramjan@gmail.com>
2026-09-25 12:37:04 +0800

    compelted login and cookie handle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Provider from "../provider";
+import { Toaster } from "@/components/ui/toast";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -35,7 +36,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       )}
     >
       <Provider>
-        <body className="min-h-full flex flex-col">{children}</body>
+        <body className="min-h-full flex flex-col">{children}
+        <Toaster/>
+        </body>
       </Provider>
     </html>
   );
--- src/components/form/login-form.tsx
+++ src/components/form/login-form.tsx
@@ -6,9 +6,17 @@ import { Field, FieldError, FieldGroup, FieldLabel } from "../ui/field";
 import { loginSchema } from "@/validation/auth.validation";
 import { useState } from "react";
 import { Eye, EyeOffIcon } from "lucide-react";
+import { useLogin } from "@/hooks";
+import { useRouter } from "next/navigation";
+import { toast } from "../ui/toast";
+import { Spinner } from "../ui/spinner";
 
 function LoginForm() {
   const [showPassword, setPassword] = useState(false);
+
+  const router = useRouter();
+
+  const { mutate: login, isPending: loginPending } = useLogin();
   const form = useForm({
     defaultValues: {
       email: "",
@@ -17,8 +25,31 @@ function LoginForm() {
     validators: {
       onSubmit: loginSchema,
     },
-    onSubmit: (data) => {
-      console.log(data);
+    onSubmit: ({ value }) => {
+      // console.log(data);
+      const loginData = {
+        email: value.email,
+        password: value.password,
+      };
+      login(loginData, {
+        onSuccess: (res) => {
+          toast.add({
+            title: "successfully login",
+            description: "Welcome back",
+            type: "success",
+          });
+          // console.log(res);
+          router.push("/");
+        },
+        onError: (err) => {
+          toast.add({
+            title: "Authorization failure",
+            description: err.message || "something went wronge",
+            type: "error",
+          });
+          console.log(err);
+        },
+      });
     },
   });
 
@@ -65,29 +96,35 @@ function LoginForm() {
                   <FieldLabel htmlFor={field.name}>Password</FieldLabel>
                   <div className=" relative">
                     <Input
-                    id={field.name}
-                    type={showPassword ? "text" : "password"}
-                    name={field.name}
-                    autoComplete="off"
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    aria-invalid={isInvalid}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  <button
-                  className=" absolute right-3 top-1/2 -translate-y-1/2"
-                    type="button"
-                    onClick={() => setPassword((prev) => !prev)}
-                  >
-                    {showPassword ? <Eye size={20}/> : <EyeOffIcon size={20} />}
-                  </button>
+                      id={field.name}
+                      type={showPassword ? "text" : "password"}
+                      name={field.name}
+                      autoComplete="off"
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      aria-invalid={isInvalid}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                    <button
+                      className=" absolute right-3 top-1/2 -translate-y-1/2"
+                      type="button"
+                      onClick={() => setPassword((prev) => !prev)}
+                    >
+                      {showPassword ? (
+                        <Eye size={20} />
+                      ) : (
+                        <EyeOffIcon size={20} />
+                      )}
+                    </button>
                   </div>
                   {isInvalid && <FieldError errors={field.state.meta.errors} />}
                 </Field>
               );
             }}
           </form.Field>
-          <Button type="submit">Submit</Button>
+          <Button disabled={loginPending} type="submit">
+            {loginPending?<><Spinner/>Submiting...</>: <>Submit</>}
+          </Button>
         </FieldGroup>
       </form>
     </div>
